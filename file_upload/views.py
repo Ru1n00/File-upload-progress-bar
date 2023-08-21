7e18adac2ee048c5dd8e7f7b5178e196a6dc77c6
Ru1n00 <ruhul.amin.tnt@gmail.com>
2023-08-21 20:01:53 +0600

    Create upload file  view and templates

--- file_upload/views.py
+++ file_upload/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
+
+from .models import UploadFile
 
 # Create your views here.
+class CreateUploadFile(SuccessMessageMixin, CreateView):
+    model = UploadFile
+    fields = ['file']
+    template_name = 'file_upload/upload.html'
+    success_url = reverse_lazy('file_upload:index')
+    
+    def get_success_message(self, cleaned_data):
+        return 'File uploaded successfully'
