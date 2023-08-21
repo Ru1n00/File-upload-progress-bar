bee853922d922aaa2574e4e558aa76d749153236
Ru1n00 <ruhul.amin.tnt@gmail.com>
2023-08-21 19:45:35 +0600

    Register uploadfile model in admin

--- file_upload/admin.py
+++ file_upload/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UploadFile
 
 # Register your models here.
+class UploadFileAdmin(admin.ModelAdmin):
+    list_display = ('file', 'uploaded_at')
+
+admin.site.register(UploadFile, UploadFileAdmin)
